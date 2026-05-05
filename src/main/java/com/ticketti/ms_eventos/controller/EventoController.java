9c2809dffd4206ee92513ff9614ef07ac89a479b
marceloriv <ma.riveracontreras@duocuc.cl>
2026-05-05 00:05:55 -0400

    refactor(EventoController): reorganizar importaciones y mejorar comentarios

--- src/main/java/com/ticketti/ms_eventos/controller/EventoController.java
+++ src/main/java/com/ticketti/ms_eventos/controller/EventoController.java
@@ -2,7 +2,14 @@ package com.ticketti.ms_eventos.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +18,7 @@ import com.ticketti.ms_eventos.model.Evento;
 import com.ticketti.ms_eventos.model.Genero;
 import com.ticketti.ms_eventos.service.EventoService;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Controlador REST para operaciones CRUD de Eventos.
@@ -28,6 +28,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequiredArgsConstructor
 public class EventoController {
 
+    @Autowired
     private EventoService eventoService;
 
     /**
@@ -80,8 +81,8 @@ public class EventoController {
     }
 
     /**
-     * Método para que cuando se compre una entrada, se reste del stock.
-     * Este método es consumido por RabbitMQ y por lógica.
+     * Método para que cuando se compre una entrada, se reste del stock. Este
+     * método es consumido por RabbitMQ y por lógica.
      */
     @PatchMapping("/actualizarStock/{id}/{cantidad}")
     public ResponseEntity<Void> actualizarStock(@PathVariable Integer id, @PathVariable Integer cantidad) {
